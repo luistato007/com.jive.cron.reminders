b812882c6f32d5e028a069f14326bf8301a9ecb7
luistato007 <felipemende@gmail.com>
2017-01-17 16:45:16 -0700

    funtionality of cron

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-console.log("Hola Mundo");
 var cron = require('node-cron');
-
 var Client = require('node-rest-client').Client;
- 
+var admin = require("firebase-admin");
+const util = require('util');
+
 var client = new Client();
 
-var message = "test cron";
-var participants = ["4243219814"];
-var author = "+13852690621";
-
-var args = {
-    data: {
-        body: message,
-        recipients: participants,
-        author: author
-    },
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'bearer eyJhbGciOiJIUzUxMiJ9.eyJzY3AiOiJqaXZlLnYxLnVuZGVmaW5lZCIsInN1YiI6ImZtZW5kZXoiLCJsZGFwIjoiUGxhdGZvcm0tQWRtaW4sIFBsYXRmb3JtLUFkbWluLCBQbGF0Zm9ybS1DdXN0b21lci1TZXJ2aWNlIiwiaXNzIjoiZGVmYXVsdCIsInR5cCI6ImFjY2VzcyIsImdudCI6ImZlNzhiYTVhLTE5YmEtNDE2Zi1iYzgwLTI1MDI5ZWQ3ZTEzMiIsImV4cCI6MTQ4NTE5MTA5MSwianRpIjoiNDMwODZmNTktYTJiYi00Yjg0LTgxYmQtYTJmZDVmZmIxMDAzIiwiY2lkIjoiNWYzMzc1ZjUtYzhmZi00M2UyLWE3NzAtODIxZDY1YzA3NzY4In0.NPyo_aGqtAjNo6WBa1bsjRdZzod4_z2whn-MkDBOz0ZRt9rpme71nNDiuNd7wqG8gTscYV7CgC8SZrFHMtdXcQ'
-    }
-};
+admin.initializeApp({
+    credential: admin.credential.cert("credentials/jive-chat-firebase-adminsdk-n0zsp-c33bd2f54f.json"),
+    databaseURL: "https://jive-chat.firebaseio.com"
+});
 
 cron.schedule('* * * * *', function(){
-    client.post("https://api.jive.com/chat/v1/message", args, function (data, response) {
-        // parsed response body as js object 
-        console.log("data: " + JSON.stringify(data));
+    console.log('cron');
+    var events = admin.database().ref('events');
+    events.orderByChild("timestamp").on("child_added", function(snapshot){
+        var event = snapshot.val();
+        
+        if (!event['sent']) {
+
+            var d = new Date();
+            var n = d.getTime();
+
+            var difference = Math.floor((event['timestamp'] - n)/1000/60);
+            console.log(n);
+            console.log(event['timestamp']);
+            console.log(difference);
+            if (difference > 0 && difference < 30) {
+
+                var participants = event['participants'].split(',');
+
+                var args = {
+                    data: {
+                        body: event['title'] + " : " + event['description'],
+                        recipients: participants,
+                        author: event['phone']
+                    },
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'bearer eyJhbGciOiJIUzUxMiJ9.eyJzY3AiOiJqaXZlLnYxLnVuZGVmaW5lZCIsInN1YiI6ImZtZW5kZXoiLCJsZGFwIjoiUGxhdGZvcm0tQWRtaW4sIFBsYXRmb3JtLUFkbWluLCBQbGF0Zm9ybS1DdXN0b21lci1TZXJ2aWNlIiwiaXNzIjoiZGVmYXVsdCIsInR5cCI6ImFjY2VzcyIsImdudCI6IjQyYmRiM2MxLWM3ZTktNGRlYy1hMjgyLTA2MzlhZjg5ZTM3MyIsImV4cCI6MTQ4NTI5OTc0NCwianRpIjoiMDg3MWM5OTEtMDZhZC00NjJlLThlODctY2Y1OTNiOGYxMDNiIiwiY2lkIjoiNWYzMzc1ZjUtYzhmZi00M2UyLWE3NzAtODIxZDY1YzA3NzY4In0.XR9zxvqlPMaPwI6PcgE4zP18y3mBJDY1flewL03dKFZaoJswZynPH21LjkCZTmPt5TcFwKM0XZTw5ArihykwEQ'
+                    }
+                };
+
+                console.log(args);
+
+                client.post("https://api.jive.com/chat/v1/message", args, function (data, response) {
+                    console.log("data: " + JSON.stringify(data));
+                });
+
+                event['sent'] = true;
+                var updates = {};
+                updates['/events/' + snapshot.key] = event;
+                admin.database().ref().update(updates); 
+            }
+        }
+
     });
-    console.log('running a task every minute');
 });
